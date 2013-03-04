5400d84046e907bb0bcaa934ce9ba024ff056ff1
Romeo Mariani <romeo.mariani@valxer.it>
2013-03-04 11:02:00 +0100

    sdsdds

--- Test/VXAppDelegate.m
+++ Test/VXAppDelegate.m
@@ -13,6 +13,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    //idg kusdy
     return YES;
 }
 							
